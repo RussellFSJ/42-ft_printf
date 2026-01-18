59bc4e78d960c07d975f5cdedaf240eb6625ca7c
Russell Foo <iruss1337@gmail.com>
2026-01-19 06:02:06 +0800

    feat: add print_hex as reusable function to use between handle_lowerhex, handle_upperhex and handle_ptr

---
+++ print_hex.c
@@ -0,0 +1,22 @@
+#include "ft_printf.h"
+
+int	print_hex(unsigned long n, int uppercase)
+{
+	const char	*hex;
+	char		buffer[17];
+	int			i;
+
+	hex = "0123456789abcdef";
+	if (uppercase)
+		hex = "0123456789ABCDEF";
+	i = 16;
+	buffer[i] = '\0';
+	if (n == 0)
+		return (write(1, "0", 1));
+	while (n > 0)
+	{
+		buffer[--i] = hex[n % 16];
+		n /= 16;
+	}
+	return (write(1, &buffer[i], 16 - i));
+}
