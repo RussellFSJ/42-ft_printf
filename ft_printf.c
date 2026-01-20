23e312f6a76a21a013f0491d7189719dc7b2bd2f
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-01-20 16:46:21 +0800

    feat: introduce handle_trailing_percent to clean up dict for s ending with %

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,14 @@
 /*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 11:58:48 by rfoo              #+#    #+#             */
-/*   Updated: 2026/01/20 15:13:35 by rfoo             ###   ########.fr       */
+/*   Updated: 2026/01/20 16:45:09 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 static int	use_handler(char s, t_dict *dict, va_list *args);
+static int	handle_trailing_percent(t_dict *dict, va_list *args, int bytes);
 
 int	ft_printf(const char *s, ...)
 {
@@ -30,7 +31,7 @@ int	ft_printf(const char *s, ...)
 		if (s[i] == '%')
 		{
 			if (!s[i + 1])
-				return (bytes + write(1, "%", 1));
+				return (handle_trailing_percent(dict, &args, bytes));
 			i++;
 			bytes += use_handler(s[i], dict, &args);
 		}
@@ -51,4 +52,11 @@ static int	use_handler(char s, t_dict *dict, va_list *args)
 	if (handler)
 		return (handler(args));
 	return (write(1, "%", 1) + write(1, &s, 1));
+}
+
+static int	handle_trailing_percent(t_dict *dict, va_list *args, int bytes)
+{
+	va_end(*args);
+	dict_free(dict);
+	return (bytes + write(1, "%", 1));
 }
